b3043ac3d98ca4d265317dee20b8666f85d78df4
Asude Ebrar Kızıloğlu <83813907+ebrarkiziloglu@users.noreply.github.com>
2021-11-21 21:32:56 +0300

    Create Solutions.py

---
+++ Solutions.py
@@ -0,0 +1,56 @@
+## Question 1
+
+odd_list = []
+even_list = []
+m = input().split()
+for e in m:
+    x = int(e)
+    if x % 2 == 0:
+        even_list.append(x)
+    else:
+        odd_list.append(x)
+
+print("odd numbers: ", odd_list)
+print("even numbers: ", even_list)
+
+## Question 2
+
+def sum_of_evens(lst):
+    result = 0
+    for e in lst:
+        if int(e) % 2 == 0:
+            result += int(e)
+    return result
+
+  
+m = input().split()
+print(sum_of_evens(m))
+
+## Question 3
+
+n = int(input())
+result = 1
+if n < 0:
+    print("invalid input")
+else:
+    while(n > 0):
+        result *= n
+        n -= 1
+    print(result)
+
+## Question 4
+
+x = int(input())
+y = int(input())
+if y > x:
+    temp = y
+    y = x
+    x = temp
+i = 1
+res = 1
+while i <= y:
+    if x % i == 0 and y % i == 0:
+        res = i
+    i += 1
+
+print(res)
